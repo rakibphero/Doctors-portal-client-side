a1852d087ac9aed535533fa9eeafe1c5dcedffa2
rakibphero <rakibrowshoncse25@gmail.com>
2022-05-22 04:51:53 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Pages/Shared/Navbar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
+import Login from './Pages/Login/Login';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
