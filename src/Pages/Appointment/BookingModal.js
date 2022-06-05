8ba27b177c84ad4f5d634985c571cab82380c5bf
rakibphero <rakibrowshoncse25@gmail.com>
2022-06-06 01:18:29 +0600

    signinwithgoogle added

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -14,16 +14,16 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
   };
   return (
     <div>
-      <input type="checkbox" id="booking-modal" class="modal-toggle" />
-      <div class="modal modal-bottom sm:modal-middle">
-        <div class="modal-box">
+      <input type="checkbox" id="booking-modal" className="modal-toggle" />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
           <label
             for="booking-modal"
-            class="btn btn-sm btn-circle absolute right-2 top-2"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
           >
             ✕
           </label>
-          <h3 class="font-bold text-lg text-secondary">Booking for : {name}</h3>
+          <h3 className="font-bold text-lg text-secondary">Booking for : {name}</h3>
           <form
             onSubmit={handleBooking}
             className="grid grid-cols-1 gap-3 justify-items-center mt-2"
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+
+import auth from "../../firebase.init";
 
 const Login = () => {
-    return (
-        <div>
-            <h2>This is login page</h2>
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    
+    if(user){
+        console.log(user);
+    }
+
+
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="text-center text-2xl font-bold">Login</h2>
+
+          <div className="divider">OR</div>
+          <button 
+          onClick={() => signInWithGoogle()}
+          className="btn btn-outline"
+          >Continue with Google</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
