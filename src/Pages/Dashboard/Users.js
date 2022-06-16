840ad19558b612e09ee7d14eacef133ebe4de520
rakibphero <rakibrowshoncse25@gmail.com>
2022-06-17 03:11:23 +0600

    make admin and remove admin btn added

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -4,11 +4,17 @@ import Loading from "../Shared/Loading";
 import UserRow from "./UserRow";
 
 const Users = () => {
-  const { data: users, isLoading } = useQuery('users', () => fetch('http://localhost:5000/user').then(res => res.json()))
-  console.log(users);
+  const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user',{
+    method: 'GET',
+    headers: {
+      authorization: `Bearer ${localStorage.getItem('accessToken')}`
+    }
+  }).then(res => res.json()));
+
   if (isLoading) {
     return <Loading></Loading>;
   }
+  
   return (
     <div>
       <h2 className="text-2xl">All Users: {users.length}</h2>
@@ -26,8 +32,9 @@ const Users = () => {
             {
                 users && users?.map(user =><UserRow
                   key={user._id}
-                  user={user}>
-                </UserRow>)
+                  user={user}
+                  refetch={refetch}
+                  ></UserRow>)
             }
           </tbody>
         </table>
