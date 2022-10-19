2e91d78e72e40a0369c1f42e52fc0168952d63f6
rakibphero <rakibrowshoncse25@gmail.com>
2022-10-19 23:51:07 +0600

    hooks components update

--- src/hooks/useAdmin.js
+++ src/hooks/useAdmin.js
@@ -3,21 +3,21 @@ import { useEffect, useState } from "react"
 const useAdmin = user => {
     const [admin, setAdmin] = useState(false);
     const [adminLoading, setAdminLoading] = useState(true);
-    useEffect( () =>{
+    useEffect(() => {
         const email = user?.email;
-        if(email){
-            fetch(`http://localhost:5000/admin/${email}`, {
-                method:'GET',
+        if (email) {
+            fetch(`https://limitless-inlet-88208.herokuapp.com/admin/${email}`, {
+                method: 'GET',
                 headers: {
                     'content-type': 'application/json',
                     authorization: `Bearer ${localStorage.getItem('accessToken')}`
                 }
             })
-            .then(res=>res.json())
-            .then(data => {
-                setAdmin(data.admin);
-                setAdminLoading(false);
-            })
+                .then(res => res.json())
+                .then(data => {
+                    setAdmin(data.admin);
+                    setAdminLoading(false);
+                })
         }
     }, [user])
 
