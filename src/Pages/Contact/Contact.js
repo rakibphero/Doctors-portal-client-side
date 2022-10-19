7dccd0c87e2e14a31797d8d0972fd6d550deaa9e
rakibphero <rakibrowshoncse25@gmail.com>
2022-10-19 23:23:09 +0600

    contact section update

---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+
+    return (
+        <div className='mt-10 px-5 py-10 bg-gray-100'>
+            <p> <span className="font-semibold text-lg">Phone : </span> 09666-710606, 01303-115423</p>
+            <p> <span className="font-semibold text-lg">Email : </span> <span className='font-semibold text-primary text-lg'> doctorsbd@gmail.com </span> </p>
+            <p> <span className="font-semibold text-lg">For Appointment : </span> 03444-768656</p>
+            <p> <span className="font-semibold text-lg">Emergency : </span> 03444-768656</p>
+        </div>
+    );
+};
+
+export default Contact;
