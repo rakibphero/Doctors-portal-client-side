e136c124dccf9a48d1c6511c5f9e46c295e172f3
rakibphero <rakibrowshoncse25@gmail.com>
2022-10-19 23:53:53 +0600

    app.js and firebase comment added

--- src/App.js
+++ src/App.js
@@ -7,44 +7,47 @@ import Login from "./Pages/Login/Login";
 import Appointment from "./Pages/Appointment/Appointment";
 import SignUp from "./Pages/Login/SignUp";
 import RequireAuth from "./Pages/Login/RequireAuth";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 import Dashboard from "./Pages/Dashboard/Dashboard";
 import MyAppointments from "./Pages/Dashboard/MyAppointments";
 import MyReview from "./Pages/Dashboard/MyReview";
-import MyHistory from "./Pages/Dashboard/MyHistory";
 import Users from "./Pages/Dashboard/Users";
+import RequireAdmin from "./Pages/Login/RequireAdmin";
 import AddDoctor from "./Pages/Dashboard/AddDoctor";
 import ManageDoctors from "./Pages/Dashboard/ManageDoctors";
+import Payment from "./Pages/Dashboard/Payment";
+import Contact from "./Pages/Contact/Contact";
+import Doctors from "./Pages/Doctors/Doctors";
 
 function App() {
   return (
-    <div className="max-w-7xl mx-auto px-12">
+    <div className="max-w-7xl mx-auto">
       <Navbar></Navbar>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route
-          path="appointment"
-          element={
-            <RequireAuth>
-              <Appointment />
-            </RequireAuth>
-          }
-        />
-        <Route
-          path="dashboard"element={<RequireAuth><Dashboard />
-          </RequireAuth> } >
-          <Route index element={<MyAppointments></MyAppointments>} />
-          <Route path="review" element={<MyReview></MyReview>} />
-          <Route path="history" element={<MyHistory></MyHistory>} />
-          <Route path="users" element={<Users></Users>} />
-          <Route path="addDoctor" element={<AddDoctor></AddDoctor>} />
-          <Route path="manageDoctor" element={<ManageDoctors></ManageDoctors>} />
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/appointment" element={
+          <RequireAuth>
+            <Appointment></Appointment>
+          </RequireAuth>
+        } />
 
+        <Route path="/dashboard" element={<RequireAuth><Dashboard></Dashboard></RequireAuth>}>
+          <Route index element={<MyAppointments></MyAppointments>}></Route>
+          <Route path="review" element={<MyReview></MyReview>}></Route>
+          <Route path="payment/:id" element={<Payment></Payment>}></Route>
+          <Route path="users" element={<RequireAdmin><Users></Users></RequireAdmin>}></Route>
+          <Route path="addDoctor" element={<RequireAdmin><AddDoctor></AddDoctor></RequireAdmin>}></Route>
+          <Route path="manageDoctor" element={<RequireAdmin><ManageDoctors></ManageDoctors></RequireAdmin>}></Route>
         </Route>
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
+
+        <Route path="/about" element={<About></About>} />
+        <Route path="/contact" element={<Contact></Contact>} />
+        <Route path="/doctors" element={<Doctors></Doctors>} />
+        <Route path="/login" element={<Login></Login>} />
+        <Route path="/signup" element={<SignUp></SignUp>} />
       </Routes>
       <ToastContainer />
     </div>
